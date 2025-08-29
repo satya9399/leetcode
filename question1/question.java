7dff64eafe70c2e6b12f7649359e21054cd046c3
SATYAM <satyamshriwas09@gmail.com>
2025-08-29 22:31:08 +0530

    hii

---
+++ question1/question.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long flowerGame(int n, int m) {
+
+        long oddN = (n + 1) / 2;
+        long evenN = n / 2;
+        long oddM = (m + 1) / 2;
+        long evenM = m / 2;
+        long totalOddSumPairs = oddN * evenM + evenN * oddM;
+        return totalOddSumPairs;
+    }
+}
